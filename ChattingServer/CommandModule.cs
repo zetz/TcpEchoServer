2f8762aaea075880e926752c503257f60c79327d
zetz <laitdei@gmail.com>
2018-08-21 16:07:35 +0900

    dfdf

---
+++ ChattingServer/CommandModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChattingServer
+{
+	public static class CommandModule
+	{
+		private static Dictionary<string, Action<string[]>> _commands = new Dictionary<string, Action<string[]>>();
+
+		public static Task StartAsync() {
+			AddCommand("help", Command_Help);
+			return Task.Factory.StartNew(ReadInputTask);
+		}
+
+		private static void ReadInputTask()
+		{
+			while (true) {
+				var line = System.Console.ReadLine();
+				var tokens = line.Split(" ");
+				if (tokens.Length > 0) {
+					var command = tokens[0].ToLower();
+					if (command == "exit") {
+						break;
+					}
+
+					if (_commands.TryGetValue(command, out var action)) {
+						action(tokens);
+					}
+				}
+			}
+		}
+
+		public static void AddCommand(string cmd, Action<string[]> callback) {
+			_commands.Add(cmd.ToLower(), callback);	
+		}
+
+		private static void Command_Help(string[] args) {
+			System.Console.WriteLine($"prease command input [{string.Join(", ", _commands.Keys)}]");
+		}
+	}
+}
---
+++ ChattingServer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ChattingServer
+{
+	using System.Threading.Tasks;
+	using Network;
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var listenTask = SocketListener.StartAsync(8087);
+			if (listenTask == Task.CompletedTask) {
+				Console.WriteLine($"unable to open port 8087");
+				return;
+			}
+
+			//
+			CommandModule.AddCommand("clients", tokens => {
+				var clients = SocketConnection.Connections;
+				lock (clients) {
+					foreach (var s in clients) {
+						Console.WriteLine(s);
+					}
+				}
+			});
+			CommandModule.AddCommand("say", tokens => {
+				var clients = SocketConnection.Connections;
+				lock (clients) {
+					foreach (var s in clients) {
+						s.Send(string.Join(' ', tokens));
+					}
+				}
+			});
+
+			CommandModule.AddCommand("kill", tokens => {
+				if (tokens.Length > 1) {
+					var clients = SocketConnection.Connections;
+					var target = tokens[1];
+					if (target == "all") {
+						lock (clients) {
+							foreach (var s in clients) {
+								s.Disconnect();
+							}
+						}
+					} else {
+						lock (clients) {
+							foreach (var s in clients) {
+								if (s.ToString().Contains(target)) {
+									s.Disconnect();
+									break;
+								}
+							}
+						}
+					}
+				}
+			});
+
+			var inputTask = CommandModule.StartAsync();
+
+			Task.WaitAll(inputTask, listenTask);
+
+			Console.WriteLine("Shutdown ...");
+		}
+	}
+}
